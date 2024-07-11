a883febe49d11f2d7779ff928402173437c1d69b
luisa <Aprendiz@BOGDFPCGMS4135.FormacionSena.RED>
2024-07-11 17:37:48 -0500

    app

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapp.ui.theme.MyappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyappTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Paula Andrea Ortiz",
+                        ficha = 2902093,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, ficha: Int, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!, ficha: $ficha",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyappTheme {
+        Greeting("Android", ficha = 12345)
+    }
+}
